38a08b6eae2c017af127f2fc5dde61099db9d305
insha <ik@alphainfolab.com>
2026-09-02 12:07:59 +0530

    numpy added

--- test2.py
+++ test2.py
@@ -1,1 +1,31 @@
-print("this is a new file ")
+import numpy as np
+# scl=np.array([45,6])
+# scl1=np.array([45,6])
+# result=scl*scl1
+# print(result)
+
+m1=np.array([[1,2,3],[4,5,6],[7,8,9]])
+m2=np.array([[5,2,0],[7,8,6],[2,1,9]])
+# print(m1.ndim)
+# print(m1.shape)
+# print(m1)
+# d1=np.linalg.det(m1)
+# print(d1)
+# print(m1)
+# mul=m1 @ m2
+# print(mul)
+
+# values, vectors=np.linalg.eig(m1)
+# values1,vectors1=np.linalg.eig(m2)
+# print(values)
+# print(vectors)
+# print("this is m2 eigen values and vectors")
+# print(values1)
+# # print(vectors1)
+# pro=np.kron(m1,m2)
+# print(pro)
+
+a=2+3j
+print(a)
+print(a.real)
+print(a.imag)

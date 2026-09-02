8c609d6052c9a2a93f94c18ab9541b75d0946629
insha <ik@alphainfolab.com>
2026-09-02 09:39:02 +0530

    numpy code

--- test.py
+++ test.py
@@ -4,4 +4,9 @@ print("i added new line at 3:54")
 print("after 100 error this new line is added at 4:7")
 print("this is new pull")
 print("this is the second day of github")
+import numpy as np 
+scalar_no= np.array(45)
+vector_no= np.array([2,7,8])
 
+print(scalar_no)
+print(vector_no)

bf43559422a2fcb8c2d61831d993c6c503cea2e7
insha <ik@alphainfolab.com>
2026-09-03 14:24:12 +0530

    added calculus and trigno

--- trig.py
+++ trig.py
@@ -1,13 +1,43 @@
-import numpy as np
-angle=np.rad2deg(0.5236)
-# angle=np.deg2rad(30)
+# import numpy as np
+# angle=np.pi/6
+# # angle=np.deg2rad(30)
 
-s=np.sin(angle)
-c=np.cos(angle)
-t=np.tan(angle)
-print(s)
-print(c)
-print(t)
-angle1=np.deg2rad(30)
-s1=np.sin(angle1)
-print(s1)
+# # s=np.sin(angle)
+# # c=np.cos(angle)
+# # t=np.tan(angle)
+# # print(np.rad2deg(angle))
+# # print(np.sin(np.rad2deg(np.pi/2)))
+# # print(np.rad2deg(np.pi/2))
+# # print(np.sin(np.deg2rad(30)))
+# # print(np.sin(np.pi/6))
+
+# print(np.rad2deg(np.pi/6))
+
+# print(s)
+# print(c)
+# print(t)
+# angle1=np.deg2rad(30)
+# s1=np.sin(angle1)
+# print(s1)
+
+# import numpy as np
+
+# # x = np.sin(np.pi/2)
+
+# # print(x)
+# base=10
+# perp=5
+
+# hypo=np.hypot(base,perp)
+# print(hypo)
+# import math 
+# p=pow(5,2)
+# h=pow(6,2)
+# b=math.sqrt(h-p)
+# print(b)
+import numpy as np
+
+h=5
+p=2
+base=np.sqrt(h**2-p**2)
+print(base)

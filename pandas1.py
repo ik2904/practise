0e048bf30565ef381862f1e8fe270d5816b1d38e
insha <ik@alphainfolab.com>
2026-09-04 11:23:26 +0530

    series and dataframes

--- pandas1.py
+++ pandas1.py
@@ -9,11 +9,44 @@ import pandas as p
 
 # # print(myvar)
 # print(p.__version__)
+#
+
+# series__--------------------------------------------------->
+# series is like a column in a table 
+# a = [1, 7, 2]
+
+# myvar = p.Series(a,index=[10,9,8])
+
+# print(myvar)
+# print(myvar[10])
+
+# key value pair as series
+# days={"day1":"monday","day2":"tuesday","day3":"wednesday"}
+# day=p.Series(days)
+# print(day)
+# print(day["day2"])
+
+# DATA  FRAME-------------------------->>>>>>>>
+week={
+"weekday":["monday","tuesday","wednesday","thursday"],
+
+"weekend":["friday","saturday","sunday","monady"]
+
+}
+# w1=p.DataFrame(week)
+# # print(w1)
+# print(w1.loc[[1,2]])
+# # print(w1.loc[1])
 
 
 
-a = [1, 7, 2]
+# "saturday","sunday"
 
-myvar = p.Series(a,index=["10","9","8"])
+data = {
+  "calories": [420, 380, 390],
+  "duration": [50, 40, 45]
+}
 
-print(myvar)
+df = p.DataFrame(data, index = ["day1", "day2", "day3"])
+
+print(df.loc["day2"])

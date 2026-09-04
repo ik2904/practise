a0c979358b0e1e13136da3f02344ce94cacb4677
insha <ik@alphainfolab.com>
2026-09-04 12:50:42 +0530

    json added

---
+++ jsondata.py
@@ -0,0 +1,4 @@
+import pandas as pd
+show=pd.read_json("data.json")
+print(show)
+print(show.to_string())
--- load.py
+++ load.py
@@ -1,5 +1,14 @@
 import pandas as p
 csv_data=p.read_csv("data.csv")
-print(csv_data)   #it show data but from start and  middle data is hide and then show last data  
+#it show data but from start and  middle data is hide and then show last data  
 # Pandas will only return the first 5 rows, and the last 5 rows
-print(csv_data.to_string()) #it shows all data  
+print(csv_data)   
+ #it shows all data  
+# print(csv_data.to_string())
+print(p.options.display.max_rows)
+# use to increase the rows in system 
+p.options.display.max_rows = 9999
+
+df = p.read_csv('data.csv')
+
+print(df) 

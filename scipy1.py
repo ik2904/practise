ef6387dc1c5678193d099f680fe7344775f6330d
insha <ik@alphainfolab.com>
2026-09-08 16:27:03 +0530

    minimize code added

--- scipy1.py
+++ scipy1.py
@@ -1,8 +1,14 @@
 import scipy.optimize as s
 import numpy as n
 
-def eqn(x):
-    return x+n.cos(x)
+# def eqn(x):
+#     return x+n.cos(x)
 
-myroot=s.root(eqn,0)
-print(myroot.x)
+# myroot=s.root(eqn,0)
+# print(myroot.x)
+
+def eqn(x):
+    return 3*x+x**2+x+2
+
+mymin = s.minimize(eqn,0,method='BFGS')
+print(mymin)

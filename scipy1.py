a1b8f3cc6037147212dd9acc6cb21bc89af250d9
insha <ik@alphainfolab.com>
2026-09-09 09:44:40 +0530

    scipy sparse csr matrix added

--- scipy1.py
+++ scipy1.py
@@ -7,8 +7,19 @@ import numpy as n
 # myroot=s.root(eqn,0)
 # print(myroot.x)
 
-def eqn(x):
-    return 3*x+x**2+x+2
 
-mymin = s.minimize(eqn,0,method='BFGS')
-print(mymin)
+# def eqn(x):
+#     return 3*x+x**2+x+2
+
+# mymin = s.minimize(eqn,0,method='BFGS')
+#print(mymin)
+
+# compressed sparese column
+import scipy.sparse as ss
+arr=n.array([1,0,0,0,0,2,0,0,0,0,3,4,5])
+print(ss.csr_matrix(arr))
+print(ss.csr_matrix(arr).data)
+print(ss.csr_matrix(arr).count_nonzero())
+mat=ss.csr_matrix(arr)
+print(mat.eliminate_zeros())
+print(mat)

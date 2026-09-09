92708d77c9e8eaa90d7ca987862799ecefdb8f5e
insha <ik@alphainfolab.com>
2026-09-09 12:12:43 +0530

    dijkstra algo code

--- scipy1.py
+++ scipy1.py
@@ -15,13 +15,37 @@ import numpy as n
 #print(mymin)
 
 # compressed sparese column
-import scipy.sparse as ss
-arr=n.array([1,0,0,0,0,2,0,0,0,0,3,4,5])
-print(ss.csr_matrix(arr))
-print(ss.csr_matrix(arr).data)
-print(ss.csr_matrix(arr).count_nonzero())
-mat=ss.csr_matrix(arr)
-print(mat.eliminate_zeros())
-print(mat)
-cscc=ss.csr_matrix(arr).tocsc()
-print(cscc)
+# import scipy.sparse as ss
+# arr=n.array([1,0,0,0,0,2,0,0,0,0,3,4,5])
+# print(ss.csr_matrix(arr))
+# print(ss.csr_matrix(arr).data)
+# print(ss.csr_matrix(arr).count_nonzero())
+# mat=ss.csr_matrix(arr)
+# print(mat.eliminate_zeros())
+# print(mat)
+# # csr to csc 
+# cscc=ss.csr_matrix(arr).tocsc()
+# print(cscc)
+
+
+# SCIPY GRAPHS
+import scipy.sparse.csgraph as g
+import numpy as n
+import scipy.sparse as gg
+
+
+arr=n.array([
+
+
+[0,  1,  0,  0],
+[1, 0, 0, 0],
+[0, 0, 0, 1],
+[0, 0, 1, 0]
+
+    ])
+gr=gg.csr_matrix(arr)
+# gh=g.connected_components(gr)
+# print(gh)
+
+# DIJKSTRA ALGO 
+print(g.dijkstra(gr, return_predecessors=True, indices=0))

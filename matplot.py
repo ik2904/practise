b87e68eefab8eba1ddd712026183bbae7ef64d28
insha <ik@alphainfolab.com>
2026-09-10 11:28:40 +0530

    marker code added

--- matplot.py
+++ matplot.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-# xaxis=np.array([1,2,4,6,8,10,12,14,15])
+xaxis=np.array([1,2,4,6,8,10,12,14,15])
 yaxis=np.array([2,5,2,5,2,5,2,5,2])
 
-plt.plot(yaxis)
-# plt.plot(xaxis,yaxis,'o')
+# plt.plot(yaxis)
+plt.plot(xaxis,yaxis,marker='3')
 plt.show()

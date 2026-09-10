a7ccd02ec71691b5ead11a3584fa5eb276c78099
insha <ik@alphainfolab.com>
2026-09-10 14:44:35 +0530

    bar grapgh

--- matplot.py
+++ matplot.py
@@ -33,15 +33,21 @@ plt.suptitle("this is two plot")
 
 # THIS IS EXAMPLES OF SCATTER PLOT
 
-x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
-y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
-plt.scatter(x, y,color="hotpink")
+# x = np.array([5,7,8,7,2,17,2,9,4,11,12,9,6])
+# y = np.array([99,86,87,88,111,86,103,87,94,78,77,85,86])
+# plt.scatter(x, y,color="hotpink")
 
-#day two, the age and speed of 15 cars:
-x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
-y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
-plt.scatter(x,y,c='#FFBF00')
+# #day two, the age and speed of 15 cars:
+# x = np.array([2,2,8,1,15,8,12,9,7,3,11,4,7,14,12])
+# y = np.array([100,105,84,105,90,99,90,95,94,100,79,112,91,80,85])
+# plt.scatter(x,y,c='#FFBF00')
 
+
+
+# THIS IS NOW EXAMPLE OF BAR GRAPGH
+x=np.array(["student 1","student2","student3","student4","student5"])
+y=np.array([3,1,2,1,10])
+plt.bar(x,y)
 
 plt.show()
 

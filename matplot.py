dccbd0214fde09f27b7b1cd81543cdd671186b5d
insha <ik@alphainfolab.com>
2026-09-10 12:02:18 +0530

    title location add

--- matplot.py
+++ matplot.py
@@ -7,11 +7,11 @@ yaxis=np.array([2,5,2,5,2,5,2,5,2])
 plt.plot(xaxis,yaxis,marker='D',ls="dotted")
 
 
-font1 = {'color':'purple','size':10}
+font1 = {'color':'purple','size':15}
 
 
 
 plt.xlabel("distance")
 plt.ylabel("time")
-plt.title("showing distance and time",fontdict=font1)
+plt.title("showing distance and time",fontdict=font1,loc='right')
 plt.show()
